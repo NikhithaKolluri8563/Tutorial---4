6ea406627fd561d736f798200758b7b1d5639585
NikhithaKolluri8563 <54335512+NikhithaKolluri8563@users.noreply.github.com>
2019-09-16 02:05:27 -0500

    Assignment4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import {IonicApp, IonicModule, IonicErrorHandler, NavController} from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -32,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    HttpClientModule,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
+import {ContactPage} from "../contact/contact";
+import {HomePage} from "../home/home";
+
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  Fname: any;
+  password: any;
+  Lname: any;
+  email: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController, public navCtrl: NavController) {
 
   }
 
+  Signup() {
+    if (this.Fname == undefined || this.Lname == undefined || this.password == undefined || this.email == undefined) {
+
+      this.alertCtrl.create({
+        title: "Registration has been Failed",
+        message: "Please check all the fields",
+        buttons: ['ok']
+      });
+    } else {
+      localStorage.setItem("name", this.Fname);
+      localStorage.setItem("password", this.password);
+      var alertc = this.alertCtrl.create({
+        title: "Registered Successfully",
+        subTitle: "Account is Created",
+        buttons: [ {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.push(HomePage);
+          }
+        }]
+      });
+      alertc.present();
+
+    }
+  }
 }
